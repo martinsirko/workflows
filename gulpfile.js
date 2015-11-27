85adec3e4f4544f90eea4d07888a04c2c53c2fee
Martin Sirkovsky <martin@martin.fo>
2015-11-27 21:36:41 +0000

    Compass task added

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,19 @@ var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
 	browserify = require('gulp-browserify'),
+	compass = require('gulp-compass'),
 	concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/*.coffee']
+
 var jsSources = ['components/scripts/rclick.js',
 				 'components/scripts/pixgrid.js',
 				 'components/scripts/tagline.js',
 				 'components/scripts/template.js'
-					];
+				];
+
+var sassSources = ['components/sass/style.css'];
+
 
 // Process coffee files
 gulp.task('coffee', function () {
@@ -26,4 +31,15 @@ gulp.task('js', function() {
 		.pipe(concat('script.js'))
 		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass', function() {
+	gulp.src(sassSources)
+		.pipe(compass({
+			sass: 'components/sass',
+			image: 'builds/development/images',
+			style: 'expanded'
+		})
+		.on('error', gutil.log))
+		.pipe(gulp.dest('builds/development/css'))
 });
